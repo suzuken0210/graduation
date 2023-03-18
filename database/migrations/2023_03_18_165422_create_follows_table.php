04aaa3caf02b8dd5c60ab8c81d4d3060996e6db6
motoki-yokoyama <21ee438@haruka.otemon.ac.jp>
2023-03-18 17:06:07 +0900

    followsテーブルの実装

---
+++ database/migrations/2023_03_18_165422_create_follows_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->foreignId('followed_id')->constrained('users');   //参照先のテーブル名を
+            $table->foreignId('following_id')->constrained('users');    //constrainedに記載
+            $table->primary(['followed_id', 'following_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follows');
+    }
+};
